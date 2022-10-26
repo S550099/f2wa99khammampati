e44c977c1cdd95364931ebe432ee01af08752f42
S550099 <112120405+S550099@users.noreply.github.com>
2022-10-26 11:53:24 -0500

    modified users js file

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
-
+let time = 0;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
 
+router.get('/', function(req, res, next) {
+
+  time=time+1;
+
+  res.send('User accesses are:'  +(time));
+
+});
 module.exports = router;
